27b2bffa735ac2175e55e5fd06bcc50fd204f411
KiranNinave <ninavekirankn@gmail.com>
2019-10-03 17:51:09 +0530

    redux setuped

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
 
 export default class App extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Taskman</h1>
-            </div>
+            <Provider store={store}>
+                <div>
+                    <h1>Taskman</h1>
+                </div>
+            </Provider>
         );
     }
 }
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,11 @@
+const initalState = {
+    token: null,
+    user: null
+};
+
+export default (state = initalState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
